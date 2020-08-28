8c4b7a30de0bf7e2408777dac6ffa89f633226df
SMahdiFaghih <smahdifaghih2001@gmail.com>
2020-08-28 20:45:08 +0430

    BulletCollision Added

---
+++ Assets/Scripts/BulletCollision.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletCollision : MonoBehaviour
+{
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag.Contains("Gun"))
+        {
+            return;
+        }
+        Destroy(gameObject);
+        if (collision.collider.tag == "Enemy")
+        {
+            Destroy(collision.collider);
+        }
+    }
+}
