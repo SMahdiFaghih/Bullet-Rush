e7ba8fe4541ceedbe08ac1efbad8cc53cd88ea49
SMahdiFaghih <smahdifaghih2001@gmail.com>
2020-08-28 16:39:44 +0430

    create Enemies Scriptable Object

---
+++ Assets/Scripts/EnemiesData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "EnemiesData", menuName = "EnemiesData")]
+public class EnemiesData : ScriptableObject
+{
+    public List<Enemy> Enemies = new List<Enemy>();
+
+    [Serializable]
+    public class Enemy
+    {
+        public Vector3 Scale = new Vector3();
+        public int Speed;
+        public int Health;
+    }
+}
