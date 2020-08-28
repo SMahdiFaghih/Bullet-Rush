282e2d117281511231e9a5db595e3a2bb338ec1c
SMahdiFaghih <smahdifaghih2001@gmail.com>
2020-08-28 18:38:48 +0430

    added CameraController

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Vector3 Offset;
+    public GameObject Player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Player.transform.position + Offset;
+        transform.LookAt(Player.transform);
+    }
+}
