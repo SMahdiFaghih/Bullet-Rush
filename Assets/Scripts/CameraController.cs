8c144a5c9ff409ab2abe8641a597f638cd1f0cf8
SMahdiFaghih <smahdifaghih2001@gmail.com>
2020-09-05 22:21:35 +0430

    changed camera LookAt

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -6,15 +6,26 @@ public class CameraController : MonoBehaviour
 {
     public Vector3 Offset;
     private GameObject Player;
+    private Transform CameraRig;
 
     void Awake()
     {
         Player = GameObject.FindGameObjectWithTag("Player");
+        Transform[] childGameObjects = Player.GetComponentsInChildren<Transform>();
+        foreach (Transform transform in childGameObjects)
+        {
+            Debug.Log(transform.gameObject.name);
+            if (transform.gameObject.name == "CameraRig")
+            {
+                CameraRig = transform;
+                break;
+            }
+        }
     }
 
     void Update()
     {
         transform.position = Player.transform.position + Offset;
-        transform.LookAt(Player.transform);
+        transform.LookAt(CameraRig);
     }
 }
