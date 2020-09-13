137d2f03d4c9b85bf88250cd98d9317b87ad4311
SMahdiFaghih <smahdifaghih2001@gmail.com>
2020-09-13 15:39:23 +0430

    used 2 of sounds

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,10 +10,14 @@ public class PlayerController : MonoBehaviour
 
     public TextMesh BoostRemainingTimeText;
     private int SpeedBoost = 1;
-    private float SpeedBoostDuration = 5.0f;
+    private float SpeedBoostDuration = 6.0f;
     private float BoostRemainingTime;
 
     public GameObject BulletPrefab;
+
+    public AudioSource LeftGunShotSound;
+    public AudioSource RightGunShotSound;
+    public AudioSource RemainingTimeLessThan4Sound;
 
     private Rigidbody Rigidbody;
     private GameObject LeftGunFireTransform;
@@ -61,8 +65,16 @@ public class PlayerController : MonoBehaviour
             if (Input.GetAxis("Fire1") != 0)
             {
                 Rigidbody bullet = Instantiate(BulletPrefab, LeftGunFireTransform.transform.position, LeftGunFireTransform.transform.rotation).GetComponent<Rigidbody>();
-
                 bullet.velocity = transform.forward * BulletVelocity;
+
+                if (!LeftGunShotSound.isPlaying)
+                {
+                    LeftGunShotSound.Play();
+                }
+            }
+            else
+            {
+                LeftGunShotSound.Stop();
             }
 
             yield return new WaitForSeconds(0.05f);
@@ -70,11 +82,20 @@ public class PlayerController : MonoBehaviour
             if (Input.GetAxis("Fire2") != 0)
             {
                 Rigidbody bullet = Instantiate(BulletPrefab, RightGunFireTransform.transform.position, RightGunFireTransform.transform.rotation).GetComponent<Rigidbody>();
-
                 bullet.velocity = transform.forward * BulletVelocity;
+
+                if (!RightGunShotSound.isPlaying)
+                {
+                    RightGunShotSound.Play();
+                }
+            }
+            else
+            {
+                RightGunShotSound.Stop();
             }
 
             yield return new WaitForSeconds(0.05f);
+
         }
     }
 
@@ -88,6 +109,10 @@ public class PlayerController : MonoBehaviour
             BoostRemainingTimeText.gameObject.SetActive(true);
             while (BoostRemainingTime > 0f)
             {
+                if (BoostRemainingTime < 4f && !RemainingTimeLessThan4Sound.isPlaying)
+                {
+                    RemainingTimeLessThan4Sound.Play();
+                }
                 BoostRemainingTime -= Time.deltaTime;
                 BoostRemainingTimeText.text = BoostRemainingTime.ToString("0.0") + "s";
                 yield return null;
@@ -98,6 +123,7 @@ public class PlayerController : MonoBehaviour
         else
         {
             BoostRemainingTime += SpeedBoostDuration;
+            RemainingTimeLessThan4Sound.Stop();
         }
     }
 }
